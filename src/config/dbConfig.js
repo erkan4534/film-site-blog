fafa86e789b84122adeddba3d5294b495d7ab7f5
Erkan Yıldırım <erkanyildirim28@gmail.com>
2026-08-15 18:39:39 +0300

    db connection

---
+++ src/config/dbConfig.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    
+    const conn = await mongoose.connect("mongodb+srv://erkan:xILDkInPk1PtCN2l@cluster0.s7cztiv.mongodb.net/");
+    console.log(`MongoDB bağlantısı başarılı: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`MongoDB bağlantısı başarısız: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const app = require('./app.js');
+const connectDB = require('./config/dbConfig.js');
 
 const PORT = 3000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Sunucu ${PORT} portunda çalışıyor!`);
