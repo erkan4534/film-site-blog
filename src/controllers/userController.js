6ef8a6dbe4fb374c557c625018e987c81e109d91
Erkan Yıldırım <erkanyildirim28@gmail.com>
2026-08-19 22:33:48 +0300

    user added

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,5 @@
-const bcrypt = require('bcryptjs');
-const User = require('../models/User.js');
+const bcrypt = require("bcryptjs");
+const User = require("../models/User.js");
 
 const getAllUsers = async (req, res) => {
   try {
@@ -32,32 +32,29 @@ const createUser = async (req, res) => {
 };
 
 const updateUser = async (req, res) => {
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        { new: true },
-      ).select('-password');
-      if (!updatedUser) {
-        return res.status(404).json({ message: 'Kullanıcı bulunamadı' });
-      }
-      res.status(200).json(updatedUser);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    }).select("-password");
+    if (!updatedUser) {
+      return res.status(404).json({ message: "Kullanıcı bulunamadı" });
     }
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
-  
 
 const deleteUser = async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.userId);
-        if (!user) {
-        return res.status(404).json({ message: 'Kullanıcı bulunamadı' });
-        }
-        res.status(204).send();
-    } catch (error) {
-        res.status(400).json({ message: error.message });
+  try {
+    const user = await User.findByIdAndDelete(req.params.userId);
+    if (!user) {
+      return res.status(404).json({ message: "Kullanıcı bulunamadı" });
     }
+    res.status(204).send();
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 module.exports = {
