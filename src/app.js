b44c78ff2eb62a31c1c4860e876c4fc8171a1924
Erkan Yıldırım <erkanyildirim28@gmail.com>
2026-09-02 21:02:29 +0300

    added cookie parser

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const { globalLimiter, authLimiter } = require('./middleware/rateLimiter');
@@ -17,6 +19,7 @@ app.use(logger);
 app.use(globalLimiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use('/api/auth',authLimiter, authRoutes);
 app.use('/api/users', userRoutes);
